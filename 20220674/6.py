067edd81b421bcd68973d99038c2a9f060d3a83e
MIHIR <112602753+ravalmihir@users.noreply.github.com>
2022-09-02 09:53:25 +1200

    Update 6.py

--- 20220674/6.py
+++ 20220674/6.py
@@ -1,5 +1,6 @@
-#program that print the factor of any number
-
+#program that print the factor of any number.
+# it is small program but helpfull in mathematics.
+#easily know the factor of any number.
 
 num=int(input("enter the any no: "))
 print("the factor of ",num,"are: ")
