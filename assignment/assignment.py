3ba2d1ab744c820c3ac02d775f61b48a64efb386
MIHIR <112602753+ravalmihir@users.noreply.github.com>
2022-09-02 10:05:36 +1200

    Update assignment.py

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -1,13 +1,16 @@
-#program to make dictonery of team and their wins and losses
+#program to make dictonery of 2 teams and their wins and losses and gives winning percentage of team which you entered in output.
 
 #from ast import Break
 #from bdb import Breakpoint
 #from lib2to3.pgen2.token import PERCENT
 
+#it is intresting program although i felt difficulties in the logic of this program.
+#when i spend hours in the code then i got perfect output.
 
 d={}
 n=int(input("enter the number teams participated: "))
 
+#this code is done by for loop and if else statement
 for a in (0,n):
     tname=input("enter the team name:")
     won=int(input("enter the games won: "))
@@ -24,3 +27,5 @@ if t in d:
      print(PERCENT,"is winning percent of the team")
 else:
     print("you enter a wrong team name ")  
+    #code is ready
+    #this code is use in sports and games area.
